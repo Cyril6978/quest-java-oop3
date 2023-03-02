ee25bde71aa5e746893931face334e7ec07b3204
Cyril6978 <cyril.david8978@gmail.com>
2023-03-02 11:53:26 +0100

    Finito Pipo

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -12,7 +12,7 @@ public class Eagle extends Bird {
     public int getAltitude() {
         return altitude;
     }
-
+  
     public boolean isFlying() {
         return flying;
     }
@@ -21,4 +21,40 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff() {
+    System.out.println(this.getName() + " takes off in the sky.");
+  }
+
+    @Override
+    public void ascend(int altitude) {
+        this.altitude += altitude;
+        System.out.println(this.getName() +  " flies upward, altitude : " + this.altitude);
+  }
+
+   @Override
+    public void glide() {
+        this.flying = true;
+        if(this.flying){
+        System.out.println("It glides into the air.");
+        }
+    }
+
+    @Override
+    public void descend(int altitude) {
+        this.altitude -= altitude;
+        System.out.println(this.getName() +  " flies downward, altitude : " + this.altitude);
+  }
+
+    @Override
+    public void land(){
+        if(this.altitude > 1){
+             System.out.println(this.getName() + " is too high, it can't lands");
+            } else {System.out.println(this.getName() + "lands on the ground.");
+        }
 }
+}
+
+
+
